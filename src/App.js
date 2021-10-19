7551e61bd6468078f0db0c581e14858901465211
Pedro Ronaldo <pedromtravessin@gmail.com>
2021-10-18 22:19:58 -0300

    Start creation DropdownMenu Component

--- src/App.js
+++ src/App.js
@@ -5,9 +5,34 @@ import Header  from './components/homeScreen/Header'
 //import { Title } from './components/homeScreen/main/Title';
 import InfoBase from './InfoBase';
 import './App.css'
+import { DropdownMenu } from './components/homeScreen/HomeScreenHeader/DropdownMenu/DropdownMenu';
+import { Title } from './components/homeScreen/main/Title/Title';
 
 export default() => {
 
+  const [language, setLanguage] = useState('portuguese')
+
+  function translate(textId) {
+    if (language === 'english') {
+      if (textId === 'welcome_message') {
+        return 'Welcome'
+      }
+    } else if (language === 'portuguese') {
+      if (textId === 'welcome_message') {
+        return 'Bem-Vindo'
+      }
+    }
+  }
+
+  function welcomeMessage() {
+    if (language === 'english') {
+      return 'Welcome'
+    } else if (language === 'portuguese') {
+      return 'Bem-vindo'
+    }
+  }
+
+  /*
   const [movieList, setMovieList] = useState([]);
 
   useEffect (() => {
@@ -19,23 +44,38 @@ export default() => {
     
     LoadList();
   }, [])
+  */
 
   return (
     <div className="page">
-      <section className="lists">
-        {/*O Map ira iterar sobre a lista, criando um loop e fazendo sempre que houver informações*/}
-        {movieList.map((item, key) => (
+      {//<section className="lists">
+        //{/*O Map ira iterar sobre a lista, criando um loop e fazendo sempre que houver informações*/}
+        //{movieList.map((item, key) => (
 
           /* O Componente Movie ira receber duas props, o title, e o items, que e a requisição com o await*/
-          <Movies key={key} title={item.title} items={item.items}/>
-        ))}
-      </section>
-      <Header />    
-      <footer>
-        Aqui vai as informações referente ao projeto
-      </footer>
+          //<Movies key={key} title={item.title} items={item.items}/>
+        //))}
+      //</section>
+      //<Header />    
+      //<footer>
+        //Aqui vai as informações referente ao projeto
+      //</footer>
+    //</div>
+  //);
+}
+      <DropdownMenu id='language' initialValue={language} options={[
+        {
+          label: 'Português',
+          value: 'portuguese'
+        },
+        {
+          label: 'English',
+          value: 'english'
+        }
+      ]} setLanguage={setLanguage}/>
+      <Title size={1}>{translate('welcome_message')}</Title>
     </div>
-  );
+  )
 }
 
 
